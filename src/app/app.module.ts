68e9254e30904fb902efc42700479e99617e1336
aarmenta <armenttamario@gmail.com>
2018-05-28 12:13:18 -0600

    Added login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, FormControl, FormGroup, FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 //import modules
 import { AuthModule } from './auth-module/auth.module'
@@ -8,15 +9,37 @@ import { AuthModule } from './auth-module/auth.module'
 import { AppComponent } from './app.component';
 import { SidemenuComponent } from './sidemenu/sidemenu.component';
 
+
+//server modules
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule, AngularFireDatabase } from 'angularfire2/database';
+
+export const firebaseConfig = {
+  fire: {
+    apiKey: "AIzaSyBUxwERyaYEuSKUP0R5lwaslH0Mpk5udSo",
+    authDomain: "app-cc-fim.firebaseapp.com",
+    databaseURL: "https://app-cc-fim.firebaseio.com",
+    projectId: "app-cc-fim",
+    storageBucket: "app-cc-fim.appspot.com",
+    messagingSenderId: "131127876929"
+  }
+}
+
 @NgModule({
   declarations: [
     AppComponent,
     SidemenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AuthModule,
+    AngularFireModule.initializeApp(firebaseConfig.fire),
   ],
-  providers: [],
+  providers: [
+    AngularFireDatabase,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/auth-module/components/login/login.component.ts
+++ src/app/auth-module/components/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms'
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { User } from '../../models/user';
 
 @Component({
   selector: 'login',
@@ -6,5 +10,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  title = 'login';
+  public title = 'login';
+  public user: any;
+  constructor(private auth: AuthService){
+    this.title = 'Iniciar sesión';
+    this.user = new User('','','','','');
+  }
+  login() {
+    let data = this.user;
+
+    if (!data.email) {
+      return;
+    }
+
+    let credentials = {
+      email: data.email,
+      password: data.password
+    };
+    this.auth.signInWithEmail(credentials)
+      /*.then(
+        () => this.navCtrl.parent.select(0),
+        error => this.loginError = error.message
+      );*/
+      console.log(credentials);
+  }
+  
 }
